defbed83572d378ef2ca85c2b96821afaa6e4778
Guilherme Santos <agustinho_santos@hotmail.com>
2022-06-29 10:14:44 -0300

    js-churrascometro

---
+++ script.js
@@ -0,0 +1,52 @@
+
+
+let inputAdultos = document.getElementById('adultos');
+let inputCriancas = document.getElementById('criancas');
+let inputDuracao = document.getElementById('duracao');
+
+let resultado = document.getElementById('resultado');
+
+function calcular () {
+    console.log('Calculando...')
+
+    let adultos = inputAdultos.value
+    let criancas = inputCriancas.value
+    let duracao = inputDuracao.value
+
+    let qtdTotalCarnes = carnePP(duracao) * adultos + ( carnePP(duracao)/2 * criancas);
+    let qtdTotalCerveja = cervejaPP(duracao) * adultos;
+    let qtdTotalBebidas = bebidasPP(duracao) * adultos + ( carnePP(duracao)/2 * criancas);
+
+    resultado.innerHTML = `<p>${qtdTotalCarnes / 1000}Kg de Carne</p>`
+    resultado.innerHTML += `<p>${Math.ceil(qtdTotalCerveja / 355)} latas de Cerveja</p>`
+    resultado.innerHTML += `<p>${qtdTotalBebidas /1000}L de Bebidas</p>`
+
+
+
+
+
+}
+
+function carnePP (duracao) {
+    if(duracao >= 6) {
+        return 650;
+    } else {
+        return 400;
+    }
+}
+
+function cervejaPP (duracao) {
+    if(duracao >= 6) {
+        return 2000;
+    } else {
+        return 1200;
+    }
+}
+
+function bebidasPP (duracao) {
+    if(duracao >= 6) {
+        return 1000;
+    } else {
+        return 1500;
+    }
+}
